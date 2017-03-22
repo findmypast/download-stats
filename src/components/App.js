dbed225c8f16ee82a84f0cb618bd3c9b224c5c46
Richard Kotze <rkotze@dctfh.com>
2017-03-22 23:21:55 +0000

    add in notes for first step.

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { Link } from 'react-router';
 import s from '../styles/app.style';
 
+//basic stateless component
 function App() {
-
+	/* all jsx must be wrapped in a single node
+	return (
+	<div>something</div>
+	<p>other</p>
+	)
+	*/
   return (
     <div style={s.root}>
       <h1 style={s.title}>Package download stats</h1>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
+// import part of ES6
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 
+// import Main app from components
 import App from './components/App';
+// import css, handled by webpack
 import './styles/base-styles.css';
 
 const routes = (
   <Route path={homeRoute} component={App} />
 );
 
+// Render app to root div in index.html
+// render is from react-dom above ^^
 render(
   <Router
     history={browserHistory}
