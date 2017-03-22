91e12b34b06b27a7fe39df2adc61dcdffe6e8041
Richard Kotze <rkotze@dctfh.com>
2017-03-22 23:51:58 +0000

    adding props to a stateless component.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,36 @@ import React from 'react';
 import { Link } from 'react-router';
 import s from '../styles/app.style';
 
-//basic stateless component
-function App() {
-	/* all jsx must be wrapped in a single node
+/* Create stateless component
+-- Show Package Title, github link and npm link package
+-- create one for `lodash` https://www.npmjs.com/package/lodash
+-- props is an object in the first param
+props = { title, github, npmLink }
+
+function PackageDetails(props){ // React enforce capital for first charater
 	return (
-	<div>something</div>
-	<p>other</p>
-	)
-	*/
+		<div>
+			<h2>{props.title}</h2>
+			<p>
+				<a href={props.github}>Github</a> |&nbsp;
+				<a href={props.npmLink}>NPM</a>
+			</p>
+		</div>
+	);
+}
+
+PackageDetails.defaultProps = {
+	title: 'lodash',
+	github: 'https://github.com/lodash/lodash',
+	npmLink: 'https://www.npmjs.com/package/lodash'
+};
+
+-- show props and destruct props
+-- default props
+-- import & export new component
+*/
+
+function App() {
   return (
     <div style={s.root}>
       <h1 style={s.title}>Package download stats</h1>
